6bd7ef2fa96ea26ccdc248aea7c987e283db3c6c
minh110896 <hoangminh11081996@gmail.com>
2017-11-21 21:28:41 +0700

    GVTK-Update(80%)-Template(User)

--- ProjectManagement/Controllers/Admin/GiangVienController.cs
+++ ProjectManagement/Controllers/Admin/GiangVienController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using ProjectManagement.Models;
+using PagedList;
 
 namespace ProjectManagement.Controllers.Admin
 {
@@ -15,29 +16,37 @@ namespace ProjectManagement.Controllers.Admin
         private ProjectManagementEntities db = new ProjectManagementEntities();
 
         // GET: GiangVien
-        public ActionResult Index()
+        [HttpGet]
+        public ActionResult Index(string searchString, int? page, string sortOrder, string currentFilter)
         {
-            return View(db.GiangViens.ToList());
+            ViewBag.CurrentSort = sortOrder;
+            var giangvien = from b in db.GiangViens select b;
+            if (!String.IsNullOrEmpty(searchString))
+            {
+                giangvien = db.GiangViens.Where(s => s.HoTen.Contains(searchString));
+            }
+            ViewBag.SearchString = searchString;
+            if (searchString != null)
+            {
+                page = 1;
+            }
+            else
+            {
+                searchString = currentFilter;
+            }
+            ViewBag.CurrentFilter = searchString;
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(giangvien.OrderBy(s => s.HoTen).ToList().ToPagedList(pageNumber, pageSize));
         }
 
-        // GET: GiangVien/Details/5
-        public ActionResult Details(string id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            GiangVien giangVien = db.GiangViens.Find(id);
-            if (giangVien == null)
-            {
-                return HttpNotFound();
-            }
-            return View(giangVien);
-        }
+ 
 
+  
         // GET: GiangVien/Create
         public ActionResult Create()
         {
+            ViewBag.GiangVienId = new SelectList(db.GiangViens, "GiangVienId", "HoTen", "GioiTinh");
             return View();
         }
 
@@ -46,47 +55,48 @@ namespace ProjectManagement.Controllers.Admin
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create([Bind(Include = "GiangVienId,GioiTinh,HoTen")] GiangVien giangVien)
+        public ActionResult Create([Bind(Include = "GiangVienId,HoTen,GioiTinh")] GiangVien giangvien)
         {
             if (ModelState.IsValid)
             {
-                db.GiangViens.Add(giangVien);
+                db.GiangViens.Add(giangvien);
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-
-            return View(giangVien);
+            return View(giangvien);
         }
 
-        // GET: GiangVien/Edit/5
+        // GET: Nganh/Edit/5
         public ActionResult Edit(string id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            GiangVien giangVien = db.GiangViens.Find(id);
-            if (giangVien == null)
+            GiangVien giangvien = db.GiangViens.Find(id);
+            if (giangvien == null)
             {
                 return HttpNotFound();
             }
-            return View(giangVien);
+            ViewBag.GiangVienId = new SelectList(db.GiangViens, "GiangVienId", "HoTen", giangvien.GiangVienId);
+            return View(giangvien);
         }
 
-        // POST: GiangVien/Edit/5
+        // POST: Nganh/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "GiangVienId,GioiTinh,HoTen")] GiangVien giangVien)
+        public ActionResult Edit([Bind(Include = "GiangVienId,HoTen,GioiTinh")] GiangVien giangvien)
         {
             if (ModelState.IsValid)
             {
-                db.Entry(giangVien).State = EntityState.Modified;
+                db.Entry(giangvien).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
-            return View(giangVien);
+            ViewBag.GiangVienId = new SelectList(db.GiangViens, "GiangVienId", "HoTen", giangvien.GiangVienId);
+            return View(giangvien);
         }
 
         // GET: GiangVien/Delete/5
@@ -96,21 +106,21 @@ namespace ProjectManagement.Controllers.Admin
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            GiangVien giangVien = db.GiangViens.Find(id);
-            if (giangVien == null)
+            GiangVien giangvien = db.GiangViens.Find(id);
+            if (giangvien == null)
             {
                 return HttpNotFound();
             }
-            return View(giangVien);
+            return View(giangvien);
         }
 
-        // POST: GiangVien/Delete/5
+        // POST: Nganh/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
         public ActionResult DeleteConfirmed(string id)
         {
-            GiangVien giangVien = db.GiangViens.Find(id);
-            db.GiangViens.Remove(giangVien);
+            GiangVien giangvien = db.GiangViens.Find(id);
+            db.GiangViens.Remove(giangvien);
             db.SaveChanges();
             return RedirectToAction("Index");
         }
@@ -123,5 +133,7 @@ namespace ProjectManagement.Controllers.Admin
             }
             base.Dispose(disposing);
         }
+
+
     }
 }
---
+++ ProjectManagement/Controllers/User/UserController.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Helpers;
+using System.Web.Mvc;
+using System.Web.Security;
+using Microsoft.AspNet.Identity;
+//using SRVTextToImage;
+using ProjectManagement.App_Start.Classes;
+using ProjectManagement.Models;
+
+namespace ProjectManagement.Controllers.User
+{
+    public class UserController : Controller
+    {
+        private ProjectManagementEntities db = new ProjectManagementEntities();
+        //
+        // GET: /User/
+        public ActionResult Index()
+        {
+            if (UserManager.Authenticated)
+            {
+                //string checkValid = Session["userNameXaGan"].ToString();
+                if (App_Start.Classes.UserManager.Authenticated)
+                {
+                    return RedirectToAction("User/Logon", "User");
+                }
+
+                HttpCookie authCookie = System.Web.HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+                if (authCookie != null) authCookie.Expires = DateTime.Now.AddDays(-1);
+                System.Web.Security.FormsAuthentication.SignOut();
+                Session.Abandon();
+                Session.Clear();
+                Session.RemoveAll();
+
+                return RedirectToAction("User/Login");
+            }
+            else
+            {
+                return RedirectToAction("User/Login");
+            }
+        }
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")] // This is for output cache false
+        public ActionResult Login()
+        {
+            if (UserManager.Authenticated)
+            {
+                HttpCookie authCookie =
+                    System.Web.HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+                if (authCookie != null) authCookie.Expires = DateTime.Now.AddDays(-1);
+                System.Web.Security.FormsAuthentication.SignOut();
+                Session.Abandon();
+                Session.Clear();
+                Session.RemoveAll();
+            }
+            return View();
+        }
+        // POST: /User/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        [OutputCache(NoStore = true, Duration = 0, VaryByParam = "None")] // This is for output cache false
+        public ActionResult Login([Bind(Include = "Username,Password")] TaiKhoan user, string captchaText)
+        {
+            if (ModelState.IsValid)
+            {
+                string ps = Security.EncryptSha1(Security.EncryptMd5(user.Password).ToLower());
+                var login = from u in db.TaiKhoans
+                            where u.Username == user.Username && u.Password == ps
+                            select u;
+                if (login.Any())
+                {
+                    string ss = Security.EncryptSha1(Security.EncryptMd5(login.Single().Username + "#" + login.Single().Password).ToLower());
+                    Session["userNameXaGan"] = ss;
+                    this.Session.Timeout = 60;
+
+                    string data = Security.EncryptStringCbc(login.Single().Username + ";" + login.Single().TaiKhoanId, "dtuxagan");
+                    HttpCookie authCookie = FormsAuthentication.GetAuthCookie(data, false);
+                    var ticket = FormsAuthentication.Decrypt(authCookie.Value);
+
+                    if (ticket != null)
+                    {
+                        var newTicket = new FormsAuthenticationTicket(ticket.Version, ticket.Name,
+                                                                      ticket.IssueDate, ticket.Expiration,
+                                                                      ticket.IsPersistent, "");
+
+                        //Update the authCookie's Value to use the encrypted version of newTicket. 
+                        authCookie.Value = FormsAuthentication.Encrypt(newTicket);
+                    }
+                    authCookie.Expires = DateTime.Now.AddMinutes(60);
+                    Response.Cookies.Add(authCookie);
+
+                    return RedirectToAction("Index", "MainPage");
+                }
+                else
+                {
+                    ModelState.AddModelError("error", "Không tồn tại tài khoản hoặc Tài khoản chưa kích hoạt!");
+                }
+            }
+
+            return View();
+        }
+        public ActionResult Logout()
+        {
+            HttpCookie authCookie = System.Web.HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie != null) authCookie.Expires = DateTime.Now.AddDays(-1);
+            System.Web.Security.FormsAuthentication.SignOut();
+            Session.Abandon();
+            Session.Clear();
+            Session.RemoveAll();
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logon()
+        {
+            if (!UserManager.Authenticated)
+            {
+                return RedirectToAction("User/Login");
+            }
+            //decimal aCount = db.ARTICLEs.Count();
+            //ViewBag.NumberOfArticles = aCount;
+            //var listArticesLatest = db.ARTICLEs.Take(20).OrderByDescending(a => a.ARTICLE_CREATEDATE).ToList();
+            //ViewBag.listArticesLatest = listArticesLatest;
+            return View();
+        }
+    }
+}
