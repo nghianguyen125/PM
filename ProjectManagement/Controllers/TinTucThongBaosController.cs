190df733034fe880dab1023ac424b4909481d581
minh110896 <hoangminh11081996@gmail.com>
2017-11-23 10:28:01 +0700

    GiangVien

---
+++ ProjectManagement/Controllers/TinTucThongBaosController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using ProjectManagement.Models;
+
+namespace ProjectManagement.Controllers
+{
+    public class TinTucThongBaosController : Controller
+    {
+        private ProjectManagementEntities db = new ProjectManagementEntities();
+
+        // GET: TinTucThongBaos
+        public ActionResult Index()
+        {
+            var tinTucThongBaos = db.TinTucThongBaos.Include(t => t.TaiKhoan);
+            return View(tinTucThongBaos.ToList());
+        }
+
+        // GET: TinTucThongBaos/Details/5
+        public ActionResult Details(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TinTucThongBao tinTucThongBao = db.TinTucThongBaos.Find(id);
+            if (tinTucThongBao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tinTucThongBao);
+        }
+
+        // GET: TinTucThongBaos/Create
+        public ActionResult Create()
+        {
+            ViewBag.TaiKhoanId = new SelectList(db.TaiKhoans, "TaiKhoanId", "Username");
+            return View();
+        }
+
+        // POST: TinTucThongBaos/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "TinTucId,TieuDe,NoiDung,NgayDang,TaiKhoanId")] TinTucThongBao tinTucThongBao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.TinTucThongBaos.Add(tinTucThongBao);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.TaiKhoanId = new SelectList(db.TaiKhoans, "TaiKhoanId", "Username", tinTucThongBao.TaiKhoanId);
+            return View(tinTucThongBao);
+        }
+
+        // GET: TinTucThongBaos/Edit/5
+        public ActionResult Edit(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TinTucThongBao tinTucThongBao = db.TinTucThongBaos.Find(id);
+            if (tinTucThongBao == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.TaiKhoanId = new SelectList(db.TaiKhoans, "TaiKhoanId", "Username", tinTucThongBao.TaiKhoanId);
+            return View(tinTucThongBao);
+        }
+
+        // POST: TinTucThongBaos/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "TinTucId,TieuDe,NoiDung,NgayDang,TaiKhoanId")] TinTucThongBao tinTucThongBao)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tinTucThongBao).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.TaiKhoanId = new SelectList(db.TaiKhoans, "TaiKhoanId", "Username", tinTucThongBao.TaiKhoanId);
+            return View(tinTucThongBao);
+        }
+
+        // GET: TinTucThongBaos/Delete/5
+        public ActionResult Delete(decimal id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            TinTucThongBao tinTucThongBao = db.TinTucThongBaos.Find(id);
+            if (tinTucThongBao == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tinTucThongBao);
+        }
+
+        // POST: TinTucThongBaos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(decimal id)
+        {
+            TinTucThongBao tinTucThongBao = db.TinTucThongBaos.Find(id);
+            db.TinTucThongBaos.Remove(tinTucThongBao);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ ProjectManagement/js/pages/compCalendar.js
@@ -0,0 +1,97 @@
+/*
+ *  Document   : compCalendar.js
+ *  Author     : pixelcave
+ *  Description: Custom javascript code used in Calendar page
+ */
+
+var CompCalendar = function() {
+    var calendarEvents  = $('.calendar-events');
+
+    /* Function for initializing drag and drop event functionality */
+    var initEvents = function() {
+        calendarEvents.find('li').each(function() {
+            // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+            var eventObject = { title: $.trim($(this).text()), color: $(this).css('background-color') };
+
+            // store the Event Object in the DOM element so we can get to it later
+            $(this).data('eventObject', eventObject);
+
+            // make the event draggable using jQuery UI
+            $(this).draggable({ zIndex: 999, revert: true, revertDuration: 0 });
+        });
+    };
+
+    return {
+        init: function() {
+            /* Initialize drag and drop event functionality */
+            initEvents();
+
+            /* Add new event in the events list */
+            var eventInput      = $('#add-event');
+            var eventInputVal   = '';
+
+            // When the add button is clicked
+            $('#add-event-btn').on('click', function(){
+                // Get input value
+                eventInputVal = eventInput.prop('value');
+
+                // Check if the user entered something
+                if ( eventInputVal ) {
+                    // Add it to the events list
+                    calendarEvents.append('<li class="animation-slideDown">' + $('<div />').text(eventInputVal).html() + '</li>');
+
+                    // Clear input field
+                    eventInput.prop('value', '');
+
+                    // Init Events
+                    initEvents();
+                }
+
+                // Don't let the form submit
+                return false;
+            });
+
+            /* Initialize FullCalendar */
+            var date = new Date();
+            var d = date.getDate();
+            var m = date.getMonth();
+            var y = date.getFullYear();
+
+            $('#calendar').fullCalendar({
+                header: {
+                    left: 'prev,next',
+                    center: 'title',
+                    right: 'month,agendaWeek,agendaDay'
+                },
+                firstDay: 1,
+                editable: true,
+                droppable: true,
+                drop: function(date, allDay) { // this function is called when something is dropped
+
+                    // retrieve the dropped element's stored Event Object
+                    var originalEventObject = $(this).data('eventObject');
+
+                    // we need to copy it, so that multiple events don't have a reference to the same object
+                    var copiedEventObject = $.extend({}, originalEventObject);
+
+                    // assign it the date that was reported
+                    copiedEventObject.start = date;
+
+                    // render the event on the calendar
+                    // the last `true` argument determines if the event "sticks" (http://arshaw.com/fullcalendar/docs/event_rendering/renderEvent/)
+                    $('#calendar').fullCalendar('renderEvent', copiedEventObject, true);
+
+                    // remove the element from the "Draggable Events" list
+                    $(this).remove();
+                },
+                events: [
+                    {
+                        title: 'Gaming Day',
+                        start: new Date(y, m, 1),
+                        color: '#9b59b6'
+                    }
+                ]
+            });
+        }
+    };
+}();
